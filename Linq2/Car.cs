964d7090f7b7ca2bb205064b334a6b7fdd6fdc3d
Егор <egor.zholobov@yandex.ru>
2022-02-11 13:24:08 +0300

    Добавил класс машины

---
+++ Linq2/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq2
+{
+    internal class Car
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int power;
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+        private Person owner;
+        public Person Owner
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+
+    }
+}
