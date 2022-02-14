4df075139ab371068fd3eae0fba2329e7ed53006
Егор <egor.zholobov@yandex.ru>
2022-02-14 13:22:25 +0300

    Реализация карт и метода по их перемешиванию.

---
+++ LinqCards/ExtensionClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqCards
+{
+    public static class ExtensionClass
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second) //Фасует карты так: 1 от начала 1 от середины, 2 от начала 2 от середины и так все 26.
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext()) //Будет выполняться пока в обоих коллекциях возможно перемещение к следующему элементу
+            {
+                yield return firstIter.Current; //Возвращает текущий элемент не прерывая цикл
+                yield return secondIter.Current; //Возвращает текущий элемент не прерывая цикл
+            }
+        }
+       
+    }
+}
--- LinqCards/Program.cs
+++ LinqCards/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace LinqCards
 {
+
     internal class Program
     {
+        static IEnumerable<string> Suits()
+        {
+            yield return "крести";
+            yield return "бубны";
+            yield return "червы";
+            yield return "пики";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "двойка";
+            yield return "тройка";
+            yield return "четвёрка";
+            yield return "пятёрка";
+            yield return "шестёрка";
+            yield return "семёрка";
+            yield return "восьмёрка";
+            yield return "девятка";
+            yield return "десятка";
+            yield return "валет";
+            yield return "дама";
+            yield return "король";
+            yield return "туз";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var startingDeck = from s in Suits() //Несколько фром дают запрос SelectMany и сочетание каждого первого элемента с каждым вторым
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            //var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank })); //Можно было записать и так
+
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine("Масть: {0,-8} Ранг: {1,-10}",card.Suit,card.Rank);
+            }
+
+            var top = startingDeck.Take(26); //Берёт 26 первых элементов
+            var bottom = startingDeck.Skip(26); //Берёт 26 последних элементов
+            var shuffle = top.InterleaveSequenceWith(bottom); //Одна итерация перемешивания
+
+            Console.WriteLine("\n После одного перемешивания: ");
+            Console.WriteLine("");
+
+            foreach (var card in shuffle)
+            {
+                Console.WriteLine("Масть: {0,-8} Ранг: {1,-10}", card.Suit, card.Rank);
+            }
+
         }
     }
 }
